181a1894d7ed131ac1efd22367ac4273a70bfdd1
Aaditya Mehta <aadityamta@gmail.com>
2026-07-06 16:03:46 +0530

    Added middleware

--- internal/router/router.go
+++ internal/router/router.go
@@ -4,13 +4,26 @@ import (
 	"net/http"
 
 	"github.com/Aadi022/Backend_Golang/internal/handler"
+	"github.com/Aadi022/Backend_Golang/internal/middleware"
 )
 
-// Constructor function that creates and returns the router
 func New(health *handler.HealthHandler) http.Handler {
-	//http.NewServeMux() creates a new HTTP request multiplexer (router) that maps URL paths to their corresponding handler functions.
 	mux := http.NewServeMux()
-	mux.HandleFunc("/health", health.Health)
-	mux.HandleFunc("/ready", health.Ready)
+
+	healthHandler := middleware.Recovery(
+		middleware.Logging(
+			http.HandlerFunc(health.Health),
+		),
+	)
+
+	readyHandler := middleware.Recovery(
+		middleware.Logging(
+			http.HandlerFunc(health.Ready),
+		),
+	)
+
+	mux.Handle("/health", healthHandler)
+	mux.Handle("/ready", readyHandler)
+
 	return mux
 }
